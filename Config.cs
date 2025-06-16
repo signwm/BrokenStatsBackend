e2015de4b28bc362f02f6fd3328e137d51b59d94
gf716 <games.fanatic716@silomails.com>
2025-06-16 18:01:54 +0200

    Updated prices for syngs and trash

--- Config.cs
+++ Config.cs
@@ -3,42 +3,37 @@ namespace BrokenStatsBackend
     public static class Config
     {
         public static readonly string PlayerName = "Sign";
-
-        // Example mapping of synergetic suffix prices
         public static readonly Dictionary<string, int> SynergeticSuffixPrices = new()
         {
-            ["Duchów"] = 10,
-            ["Kłów"] = 15,
-            ["Szponów"] = 12,
-            ["Węży"] = 18,
-            ["Smoków"] = 25,
-            ["Vorlingów"] = 20,
-            ["Lodu"] = 14,
-            ["Orków"] = 16,
-            ["Władców"] = 30,
-            ["Torisa"] = 22,
-            ["Medy"] = 17,
-            ["Venegilda"] = 28
+            ["Duchów"] = 20250,
+            ["Kłów"] = 36000,
+            ["Szponów"] = 66000,
+            ["Węży"] = 66000,
+            ["Smoków"] = 300000,
+            ["Vorlingów"] = 700000,
+            ["Lodu"] = 700000,
+            ["Orków"] = 2000000,
+            ["Władców"] = 4500000,
+            ["Torisa"] = 4500000,
+            ["Medy"] = 7200000,
+            ["Venegilda"] = 12750000
         };
+        public const int SynergeticDefaultPrice = 11250;
 
-        // Price for synergetics not matching any suffix above
-        public const int SynergeticDefaultPrice = 5;
-
-        // Example mapping of trash item quality (roman numerals) to price
         public static readonly Dictionary<string, int> TrashQualityPrices = new()
         {
-            ["I"] = 1,
-            ["II"] = 2,
-            ["III"] = 3,
-            ["IV"] = 4,
-            ["V"] = 5,
-            ["VI"] = 6,
-            ["VII"] = 7,
-            ["VIII"] = 8,
-            ["IX"] = 9,
-            ["X"] = 10,
-            ["XI"] = 11,
-            ["XII"] = 12
+            ["I"] = 0,
+            ["II"] = 30,
+            ["III"] = 100,
+            ["IV"] = 500,
+            ["V"] = 800,
+            ["VI"] = 25000,
+            ["VII"] = 8000,
+            ["VIII"] = 25000,
+            ["IX"] = 75000,
+            ["X"] = 100000,
+            ["XI"] = 100000,
+            ["XII"] = 100000
         };
         // Price for trash with unknown quality
         public const int TrashDefaultPrice = 0;
